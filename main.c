7590b52b1bdc3ae8b2c20569d8e823ec36f1d99c
Kill Man <Kill-Man@users.noreply.github.com>
2017-06-22 12:54:53 -0500

    Add code to free toks.

--- main.c
+++ main.c
@@ -14,6 +14,7 @@ int main(int argc, char **argv)
 {
     char  *line = (char *) malloc(sizeof(char)); // String to hold the line from a file or input string.
     char **toks = (char **) malloc(sizeof(char *)); // Array of strings to hold the individual tokens of *line.
+    int i;
     *toks = (char *) malloc(sizeof(char));
     *toks = NULL; // needed in order to use tokenize ??
     printf("Killisp v%d.%d.%d\n", MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION);
@@ -21,21 +22,21 @@ int main(int argc, char **argv)
         printf("> ");
         dynamic_gets(&line, "FATAL: Error #1: Unable to allocate memory for line. Exiting.", 1); // working on mac/win/lin
         parser_init(0, 8, 1, 1, 0); // working on mac/win/lin
-        tokenize(&toks, line, "FATAL: Error #2: Unable to allocate memory for token or token_set", 2); // broken on mac (will compile everywhere else but not work)
-        /*
-        puts(*toks);
-        puts(*(toks + 1));
-        puts(*(toks + 2));
-        //*/
-        //*
-        int i;
+        tokenize(&toks, line, "FATAL: Error #2: Unable to allocate memory for token or token_set", 2); // working on mac
         for (i = 0; *(toks + i); i++) {
             puts(*(toks + i));
         }
-        //*/
-        // puts(line);
     } while (strcmp(line, "(exit)")); // While line != "(exit)"
 
     free(line);
+    for (i = 0; ; i++) {
+        if (*(toks + i) == NULL) {
+            free(*(toks + i));
+            break;
+        } else {
+            free(*(toks + i));
+        }
+    }
+    free(toks);
     return 0;
 }
