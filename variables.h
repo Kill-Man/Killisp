fcbc69ba952af814becc81e570c1bfe86290c59f
Kill Man <Kill-Man@users.noreply.github.com>
2017-06-29 13:32:53 -0500

    Moving commit

--- variables.h
+++ variables.h
@@ -1,4 +1,22 @@
 #ifndef _VARIABLES_H_
 #define _VARIABLES_H_
 
+enum {
+    INT,
+    DOUBLE,
+    CHAR_STRING
+} datatypes;
+
+typedef union variable {
+    long long long_long;
+    long double long_double;
+    char *char_pointer;
+} variable;
+
+// puts a datatype variable in key of value.
+void put_var(const char *key, const char *value, const char *datatype);
+
+//
+
+
 #endif
